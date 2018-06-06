32cf1da0300d8ce27c8eae41fa2564705bb0cdf0
T-rav <tmfrisinger@gmail.com>
2018-06-06 13:49:57 +0200

    some light refactoring of the code, added more comments on my test decision making process

--- test/DiggingPlanner.spec.js
+++ test/DiggingPlanner.spec.js
@@ -1,6 +1,6 @@
 describe("HoleDiggingPlanner", function () {
 	describe("pickNewDistance",function(){
-		describe("when 1 new distance", function(){
+		describe("Given 1 new distance", function(){
 			it("When new distance less than old expect it to be picked", function() {
 				// arrange
 				var diggingPlanner = new DiggingPlanner();
@@ -38,8 +38,8 @@ describe("HoleDiggingPlanner", function () {
 			});
 		});
 
-		describe("when 2 new distance", function(){
-			it("When both distances only require new holes, expect larger distance to be picked", function() {
+		describe("Given 2 new distances", function(){
+			it("When both distances only require new holes, expect larger distance to be picked since it requires less work", function() {
 				// arrange
 				var diggingPlanner = new DiggingPlanner();
 				// act
@@ -56,13 +56,13 @@ describe("HoleDiggingPlanner", function () {
 			// I then ignored this test and started to extract a DiggingPlanner responsiblity
 			// that brought under test to help narrow the scope of the problem to be solved
 			// I then was able to easily slot my new work in to the existing code.
-			// I ignored the need to use DI and simply newed it up, reducing the amount of test 
+			// I ignored the need to use DI and simply newed it up thus reducing the amount of test 
 			// rework that had to be done.
-			it("When both require same amount of work, first new distance be picked", function() {
+			it("When both require same amount of work, expect first new distance be picked because it has a lower position in the collection", function() {
 				// arrange
 				var diggingPlanner = new DiggingPlanner();
 				// act (both require 10 actions)
-				var actual = diggingPlanner.withLength(120)
+				var actual = diggingPlanner.withLength(240)
 											.withOldDistance(40)
 											.withNewDistance(15)
 											.withNewDistance(10)
